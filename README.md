444c570d6433913dddb707ba982a464fcca98d89
HERI <68097716+heririta@users.noreply.github.com>
2022-11-22 21:40:54 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# book_app
+# buku_app
 
 A new Flutter project.
 
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:buku_app/controllers/book_controls.dart';
+import 'package:buku_app/view/index_view.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => BookControl(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const ViewDetail(),
+      ),
+    );
+  }
+}
